a1dfa89fe5bc7aceca7fcc9242d10604ca783196
jan-blockbites <jan@blockbit.es>
2024-11-04 14:17:00 +0100

    new version

---
+++ src/pages/dasboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-500">
+      <h1 className="text-4xl text-white font-bold">Dashboard!</h1>
+    </div>
+  );
+}
+
+export default Dashboard;
